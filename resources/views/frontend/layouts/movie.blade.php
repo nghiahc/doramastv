be235774406f231fa8595baca9957198ac00404a
nghiahc <nghiahc@geniee.co.jp>
2019-06-07 10:23:36 +0700

    Maintenance

--- resources/views/frontend/layouts/movie.blade.php
+++ resources/views/frontend/layouts/movie.blade.php
@@ -86,5 +86,8 @@
     @stack('after-scripts')
 
     @include('includes.partials.ga')
+    <div class="maintenance">
+        <img src="{{ asset('img/frontend/maintenance.jpg') }}">
+    </div>
     </body>
     </html>
